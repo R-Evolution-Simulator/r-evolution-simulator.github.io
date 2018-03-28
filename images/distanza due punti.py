21778f4a36516418e137f75169347d24c71aabed
(R)Evolution Simulator <revolution.simulator@gmail.com>
2018-03-28 22:37:27 +0200

    Add files via upload

---
+++ images/distanza due punti.py
@@ -0,0 +1,9 @@
+﻿print("")
+print("Ti calcolerò la distanza tra due punti sul piano cartesiano")
+x1 = float(input("inserire l'ascissa di un punto: "))
+y1 = float(input("inserire l'ordinata del punto: "))
+x2 = float(input("inserire l'ascissa di un altro punto: "))
+y2 = float(input("inserire l'ordinata del punto: "))
+distanza = ((x1-x2)**2+(y1-y2)**2)**0.5
+#print("la distanza tra il punto ({0},{1}) e il punto ({2},{3}) è {4}".format(x1,y1,x2,y2,distanza))
+print("la distanza tra il punto ({0},{1}) e il punto ({2},{3}) è {4}".format(x1,y1,x2,y2,"Sigismondo Pandolfo Malatesta"))
